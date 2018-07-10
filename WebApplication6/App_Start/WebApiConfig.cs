c5d61c533972bce2876d0a56f51746e99a0ce7fe
Than Ke <nkthan@tma.com.vn>
2018-07-10 18:00:50 +0700

    commit code web app

---
+++ WebApplication6/App_Start/WebApiConfig.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using WebApplication6.Entities;
+
+namespace WebApplication6
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+
+            //builder.EntitySet<Product>("Product");
+            //builder.EntitySet<Category>("Category");
+            builder.EntitySet<Login>("Logins");
+            /*
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            ); */
+
+            config.MapODataServiceRoute(
+            routeName: "odata",
+            routePrefix: "api",
+            model: builder.GetEdmModel());
+
+
+            //config.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+
+            /* now the default setting for WebAPI OData is:
+            client can’t apply $count, $orderby, $select, $top, $expand, $filter in the query, query
+            like localhost\odata\Customers?$orderby=Name will failed as BadRequest,
+            because all properties are not sort-able by default, this is a breaking change in 6.0.0
+            So, we now need to enable OData Model Bound Attributes
+            */
+            config.Count().Filter().OrderBy().Expand().Select().MaxTop(null);
+
+            // WebAPI when dealing with JSON & JavaScript!  
+            // Setup json serialization to serialize classes to camel (std. Json format)  
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            // Adding JSON type web api formatting.  
+            config.Formatters.Clear();
+            config.Formatters.Add(formatter);
+        }
+        /*
+        public static Microsoft.OData.Edm.IEdmModel GetModel()
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            //builder.EntitySet<Product>("Product");
+            //builder.EntitySet<Category>("Category");
+            builder.EntitySet<Login>("Login");
+
+            return builder.GetEdmModel();
+        }
+        */
+    }
+}
---
+++ WebApplication6/Controllers/LoginsController.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.AspNet.OData;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+//using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using WebApplication6.Entities;
+
+namespace WebApplication6.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using WebApplication6.Entities;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Login>("Logins");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class LoginsController : ODataController
+    {
+        private DB_Oauth_APIEntities db = new DB_Oauth_APIEntities();
+
+        // GET: odata/Logins
+        [EnableQuery]
+        [HttpGet]
+        [Authorize]
+        public IQueryable<Login> GetLogins()
+        {
+            return db.Logins;
+        }
+
+        // GET: odata/Logins(5)
+        [EnableQuery]
+        public SingleResult<Login> GetLogin([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Logins.Where(login => login.id == key));
+        }
+
+        // PUT: odata/Logins(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Login> patch)
+        {
+            //Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Login login = db.Logins.Find(key);
+            if (login == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(login);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoginExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(login);
+        }
+
+        // POST: odata/Logins
+        public IHttpActionResult Post(Login login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Logins.Add(login);
+            db.SaveChanges();
+
+            return Created(login);
+        }
+
+        // PATCH: odata/Logins(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Login> patch)
+        {
+            //Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Login login = db.Logins.Find(key);
+            if (login == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(login);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoginExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(login);
+        }
+
+        // DELETE: odata/Logins(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Login login = db.Logins.Find(key);
+            if (login == null)
+            {
+                return NotFound();
+            }
+
+            db.Logins.Remove(login);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LoginExists(int key)
+        {
+            return db.Logins.Count(e => e.id == key) > 0;
+        }
+    }
+}
